8fa151f03dd1c07d6315034ec368f437f6f092a0
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2016-11-17 11:33:20 -0500

    batch train + read+write params

--- src/expander.py
+++ src/expander.py
@@ -26,6 +26,7 @@ class AlignmentInstance:
                 print dst_line
                 print a_line
             assert int(s)<len(self.src_words)
+
 class Expander:
     @staticmethod
     def parse_options():
@@ -46,6 +47,7 @@ class Expander:
         parser.add_option('--dst_freq_tag', dest='dst_freq_tag', help='Frequency level + tag info for source word', metavar='FILE')
         parser.add_option('--model', dest='model', help='Load/Save model file', metavar='FILE', default='model.model')
         parser.add_option('--epochs', type='int', dest='epochs', default=5)
+        parser.add_option('--batch', type='int', dest='bachsize', default=128)
         parser.add_option('--hidden', type='int', dest='hidden_units', default=200)
         parser.add_option('--hidden2', type='int', dest='hidden2_units', default=0)
         parser.add_option('--lstmdims', type='int', dest='lstm_dims', default=200)
@@ -61,72 +63,122 @@ class Expander:
         self.neg = options.neg
         assert self.neg > 0
 
-        src_embed_fp = open(options.src_embedding, 'r')
-        src_embed_fp.readline()
-        self.src_embed = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in src_embed_fp}
-        src_embed_fp.close()
-        self.src_dim = len(self.src_embed.values()[0])
-        self.src_word_dict = {word: i for i, word in enumerate(self.src_embed)}
-        self.src_embed_lookup = self.model.add_lookup_parameters((len(self.src_word_dict) + 3, self.src_dim))
-        self.src_embed_lookup.set_updated(False)
-        for word, i in self.src_word_dict.iteritems():
-            self.src_embed_lookup.init_row(i, self.src_embed[word])
-        assert '_RARE_' in self.src_word_dict
-        self.src_rare = self.src_word_dict['_RARE_']
-        print 'Loaded src word embeddings. Vector dimensions:', self.src_dim
+        if options.src_embedding != None:
+            to_save_params = []
+            src_embed_fp = open(options.src_embedding, 'r')
+            src_embed_fp.readline()
+            self.src_embed = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in src_embed_fp}
+            src_embed_fp.close()
+            self.src_dim = len(self.src_embed.values()[0])
+            self.src_word_dict = {word: i for i, word in enumerate(self.src_embed)}
+            self.src_embed_lookup = self.model.add_lookup_parameters((len(self.src_word_dict) + 3, self.src_dim))
+            self.src_embed_lookup.set_updated(False)
+            for word, i in self.src_word_dict.iteritems():
+                self.src_embed_lookup.init_row(i, self.src_embed[word])
+            assert '_RARE_' in self.src_word_dict
+            self.src_rare = self.src_word_dict['_RARE_']
+            to_save_params.append(self.src_word_dict)
+            to_save_params.append(self.src_embed_lookup)
+            print 'Loaded src word embeddings. Vector dimensions:', self.src_dim
 
-        dst_embed_fp = open(options.dst_embedding, 'r')
-        dst_embed_fp.readline()
-        self.dst_embed = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in dst_embed_fp}
-        dst_embed_fp.close()
-        self.dst_dim = len(self.dst_embed.values()[0])
-        self.dst_word_dict = {word: i for i, word in enumerate(self.dst_embed)}
-        self.dst_embed_lookup = self.model.add_lookup_parameters((len(self.dst_word_dict) + 3, self.dst_dim))
-        self.dst_embed_lookup.set_updated(False)
-        for word, i in self.dst_word_dict.iteritems():
-            self.dst_embed_lookup.init_row(i, self.dst_embed[word])
-        assert '_RARE_' in self.dst_word_dict
-        self.dst_rare = self.dst_word_dict['_RARE_']
-        print 'Loaded dst word embeddings. Vector dimensions:', self.dst_dim
+            dst_embed_fp = open(options.dst_embedding, 'r')
+            dst_embed_fp.readline()
+            self.dst_embed = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in dst_embed_fp}
+            dst_embed_fp.close()
+            self.dst_dim = len(self.dst_embed.values()[0])
+            self.dst_word_dict = {word: i for i, word in enumerate(self.dst_embed)}
+            self.dst_embed_lookup = self.model.add_lookup_parameters((len(self.dst_word_dict) + 3, self.dst_dim))
+            self.dst_embed_lookup.set_updated(False)
+            for word, i in self.dst_word_dict.iteritems():
+                self.dst_embed_lookup.init_row(i, self.dst_embed[word])
+            assert '_RARE_' in self.dst_word_dict
+            self.dst_rare = self.dst_word_dict['_RARE_']
+            to_save_params.append(self.dst_word_dict)
+            to_save_params.append(self.dst_embed_lookup)
+            print 'Loaded dst word embeddings. Vector dimensions:', self.dst_dim
 
-        pos_embed_fp = open(options.pos_embedding, 'r')
-        pos_embed_fp.readline()
-        self.pos_embed = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in pos_embed_fp}
-        pos_embed_fp.close()
-        self.pos_dim = len(self.pos_embed.values()[0])
-        self.pos_dict = {word: i for i, word in enumerate(self.pos_embed)}
-        self.pos_embed_lookup = self.model.add_lookup_parameters((len(self.pos_dict) + 3, self.pos_dim))
-        self.pos_embed_lookup.set_updated(False)
-        for word, i in self.pos_dict.iteritems():
-            self.pos_embed_lookup.init_row(i, self.pos_embed[word])
-        print 'Loaded pos word embeddings. Vector dimensions:', self.pos_dim
+            pos_embed_fp = open(options.pos_embedding, 'r')
+            pos_embed_fp.readline()
+            self.pos_embed = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in pos_embed_fp}
+            pos_embed_fp.close()
+            self.pos_dim = len(self.pos_embed.values()[0])
+            self.pos_dict = {word: i for i, word in enumerate(self.pos_embed)}
+            self.pos_embed_lookup = self.model.add_lookup_parameters((len(self.pos_dict) + 3, self.pos_dim))
+            self.pos_embed_lookup.set_updated(False)
+            for word, i in self.pos_dict.iteritems():
+                self.pos_embed_lookup.init_row(i, self.pos_embed[word])
+            to_save_params.append(self.pos_dict)
+            to_save_params.append(self.pos_embed_lookup)
+            print 'Loaded pos word embeddings. Vector dimensions:', self.pos_dim
 
-        dct = pickle.load(codecs.open(options.dst_freq_tag, 'rb'))
-        self.dst_freq_tag_dict = dict()
-        added = 0
-        for k in dct.keys():
-            for w in dct[k]:
-                if w in self.dst_word_dict:
-                    if not k in self.dst_freq_tag_dict:
-                        self.dst_freq_tag_dict[k] = []
-                    self.dst_freq_tag_dict[k].append(self.dst_word_dict[w])
-                    added+=1
-        print 'loaded dst_freq_tag_dict with classes:', len(self.dst_freq_tag_dict), 'added words:', added
+            dct = pickle.load(codecs.open(options.dst_freq_tag, 'rb'))
+            self.dst_freq_tag_dict = dict()
+            added = 0
+            for k in dct.keys():
+                for w in dct[k]:
+                    if w in self.dst_word_dict:
+                        if not k in self.dst_freq_tag_dict:
+                            self.dst_freq_tag_dict[k] = []
+                        self.dst_freq_tag_dict[k].append(self.dst_word_dict[w])
+                        added+=1
+            to_save_params.append(self.dst_freq_tag_dict)
+            print 'loaded dst_freq_tag_dict with classes:', len(self.dst_freq_tag_dict), 'added words:', added
 
-        self.src_freq_dict = dict()
-        for ln in codecs.open(options.src_freq, 'r'):
-            w,l,f = ln.split()
-            if w in self.src_word_dict:
-                self.src_freq_dict[self.src_word_dict[w]] = l
-        print 'loaded src_freq_dict with words:',len(self.src_freq_dict)
+            self.src_freq_dict = dict()
+            for ln in codecs.open(options.src_freq, 'r'):
+                w,l,f = ln.split()
+                if w in self.src_word_dict:
+                    self.src_freq_dict[self.src_word_dict[w]] = l
+            to_save_params.append(self.src_freq_dict)
+            print 'loaded src_freq_dict with words:',len(self.src_freq_dict)
 
-        inp_dim = self.src_dim+self.pos_dim
-        self.builders = [LSTMBuilder(1, inp_dim, options.lstm_dims, self.model),
-                         LSTMBuilder(1, inp_dim, options.lstm_dims, self.model)]
-        self.H1 = self.model.add_parameters((options.hidden_units, self.dst_dim + options.lstm_dims * 2))
-        self.H2 = None if options.hidden2_units==0 else self.model.add_parameters((options.hidden2_units, options.hidden_units))
-        last_hid_dims = options.hidden2_units if options.hidden2_units>0 else options.hidden_units
+            inp_dim = self.src_dim+self.pos_dim
+            self.builders = [LSTMBuilder(1, inp_dim, options.lstm_dims, self.model),
+                             LSTMBuilder(1, inp_dim, options.lstm_dims, self.model)]
+            self.hid_dim = options.hidden_units
+            self.hid2_dim = options.hidden2_units
+            self.H1 = self.model.add_parameters((self.hid_dim, self.dst_dim + options.lstm_dims * 2))
+            self.H2 = None if options.hidden2_units==0 else self.model.add_parameters((self.hid2_dim, self.hid_dim))
+            last_hid_dims = options.hidden2_units if options.hidden2_units>0 else options.hidden_units
+            self.O = self.model.add_parameters((2, last_hid_dims))
+
+            to_save_params.append(self.src_dim)
+            to_save_params.append(self.dst_dim)
+            to_save_params.append(self.pos_dim)
+            to_save_params.append(self.lstm_dims)
+            to_save_params.append(self.hid_dim)
+            to_save_params.append(self.hid2_dim)
+
+            with open(os.path.join(options.output, options.params), 'w') as paramsfp:
+                pickle.dump(to_save_params, paramsfp)
+        else:
+            self._readParams(options)
+
+    def _readParams(self, options):
+        with open(options.params, 'r') as paramsfp:
+            saved_params = pickle.load(paramsfp)
+        self.hid2_dim = saved_params.pop()
+        self.hidden_dim = saved_params.pop()
+        self.lstm_dims = saved_params.pop()
+        self.pos_dim = saved_params.pop()
+        self.dst_dim = saved_params.pop()
+        self.src_dim = saved_params.pop()
+        inp_dim = self.src_dim + self.pos_dim
+        self.builders = [LSTMBuilder(1, inp_dim, self.lstm_dims, self.model),
+                         LSTMBuilder(1, inp_dim, self.lstm_dims, self.model)]
+        self.H1 = self.model.add_parameters((self.hid_dim, self.dst_dim + self.lstm_dims * 2))
+        self.H2 = None if options.hidden2_units == 0 else self.model.add_parameters((self.hid2_dim, self.hid_dim))
+        last_hid_dims = self.hid2_dim if self.hid2_dim > 0 else self.hid_dim
         self.O = self.model.add_parameters((2, last_hid_dims))
+
+        self.src_freq_dict = saved_params.pop()
+        self.dst_freq_tag_dict = saved_params.pop()
+        self.pos_embed_lookup = saved_params.pop()
+        self.pos_dict = saved_params.pop()
+        self.dst_embed_lookup = saved_params.pop()
+        self.dst_word_dict = saved_params.pop()
+        self.src_embed_lookup = saved_params.pop()
+        self.src_word_dict = saved_params.pop()
 
     def build_graph(self, a_s):
         f_init, b_init = [b.initial_state() for b in self.builders]
@@ -172,7 +224,7 @@ class Expander:
                 err = pickneglogsoftmax(r_t, 0)
                 errs.append(err)
 
-        return esum(errs) if len(errs)>0 else None
+        return errs
 
     def train(self, src_tagged_path, dst_tagged_path, alignment_path):
         renew_cg()
@@ -184,23 +236,36 @@ class Expander:
         loss = 0
         instances = 0
         i = 0
+        errs = []
         while l1:
             i += 1
             alignment_instance = AlignmentInstance(l1, r2.readline(), a.readline(), self.src_word_dict, self.dst_word_dict, self.pos_dict, i)
-            sum_errs = self.build_graph(alignment_instance)
-            if sum_errs != None:
+            errs += self.build_graph(alignment_instance)
+            if len(errs)>options.batchsize:
+                sum_errs = esum(errs)
                 squared = -sum_errs  # * sum_errs
                 loss += sum_errs.scalar_value()
                 instances += len(alignment_instance.alignments)
                 sum_errs.backward()
                 self.trainer.update()
+                self.trainer.status()
+                print loss / instances
+                loss = 0
+                instances = 0
+                errs = []
+                renew_cg()
 
-                if i%100==0:
-                    self.trainer.status()
-                    print loss / instances
-                    loss = 0
-                    instances = 0
             l1 = r1.readline()
+        if len(errs) > 0:
+            sum_errs = esum(errs)
+            squared = -sum_errs  # * sum_errs
+            loss += sum_errs.scalar_value()
+            instances += len(alignment_instance.alignments)
+            sum_errs.backward()
+            self.trainer.update()
+            self.trainer.status()
+            print loss / instances
+            renew_cg()
 
 if __name__ == '__main__':
     (options, args) = Expander.parse_options()
