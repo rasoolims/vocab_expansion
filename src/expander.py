7890b53923795463d357ff67a8408eda16df30f5
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2016-11-21 19:50:43 -0500

    more efficient computation

--- src/expander.py
+++ src/expander.py
@@ -406,10 +406,11 @@ class Expander:
 
             best_score = float('-inf')
             best_translation = '_'
+            se = sprojector * concatenate([fw[i], bw[len(src_embed) - 1 - i]])
             for candidate in candidates:
                 tr_embed = self.dst_embed_lookup[candidate]
                 te = dprojector * tr_embed
-                se = sprojector * concatenate([fw[i], bw[len(src_embed) - 1 - i]])
+
                 score = self.cosine(se, te).npvalue()
 
                 if score>best_score:
